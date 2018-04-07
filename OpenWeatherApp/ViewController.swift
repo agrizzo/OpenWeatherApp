e8f78bfbe4e0bd300d648674a80b31e4a4d0d27a
Tony Rizzo <trizzo@koteray.com>
2018-04-07 08:09:54 -0400

    Initial Commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Tony Rizzo on 4/7/18.
+//  Copyright © 2018 Koteray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
